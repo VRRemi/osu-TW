b93be512aa217fee46ed1deceecfc35296f767e6
VRRemi <business@vrremi.xyz>
2022-01-19 13:11:39 -0800

    Update osuTW.cpp

--- osuTW/osuTW.cpp
+++ osuTW/osuTW.cpp
@@ -8,7 +8,7 @@ double TWRate { 1 };
 void DrawChangeMenu(bool isHD, bool isFL, bool isTW, double rate) {
 system("CLS");
 std::cout << "osu tw and hdfl remover\n\n";
-
+std::string hd = (isHD) ? "Remove HD [o]\n" : "Remove HD []\n"
 >
 
 }
