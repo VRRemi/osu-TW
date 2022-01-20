c89f87063acd9402ae4a44a984e8147296eeeb40
VRRemi <business@vrremi.xyz>
2022-01-19 21:53:02 -0800

    Update osuTW.cpp

--- osuTW/osuTW.cpp
+++ osuTW/osuTW.cpp
@@ -48,5 +48,6 @@ int main()
     flAddress= findSig(hProc, sigs::loadFlashlight, "xxxxxxxxxxxxx?x????xxx?xxxx?xx?xx??x?x??x");
     playerOffsets= findSig(hProc, sigs::playerOffsets, "xxxxxxx") + 0x7;
     uintptr_t playerPtr1 = findDMAAddy(hProc, playerOffsets, { 0x0 });
+    hdApproach=findSig(hProc, sigs::hdApproach, "xx??x?xx");
 
 }
